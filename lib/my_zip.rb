84ace76b9dd35bce365a937c80b2b8088209e677
Katie Hoffman <ktahoffman@gmail.com>
2015-03-23 10:45:36 -0400

    delete passing code with comment

--- lib/my_zip.rb
+++ lib/my_zip.rb
@@ -1,12 +1,1 @@
-class Array
-  def my_zip(*arrays)
-    final_array = []
-    self.each_with_index do |element, i|
-      nested_array = []
-      nested_array << element
-      arrays.each { |a| nested_array << a[i] }
-      final_array << nested_array
-    end
-    final_array
-  end
-end
+# your code goes here
