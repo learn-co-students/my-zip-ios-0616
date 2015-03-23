a59fd0e18c8bdc25c6c7d2d34efe4044f941ef52
Katie Hoffman <ktahoffman@gmail.com>
2015-03-23 10:44:10 -0400

    pass spec

--- lib/my_zip.rb
+++ lib/my_zip.rb
@@ -1,1 +1,12 @@
-# your code goes here
+class Array
+  def my_zip(*arrays)
+    final_array = []
+    self.each_with_index do |element, i|
+      nested_array = []
+      nested_array << element
+      arrays.each { |a| nested_array << a[i] }
+      final_array << nested_array
+    end
+    final_array
+  end
+end
